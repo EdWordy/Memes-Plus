10a7f9a1c1a3bd2dbabc09c75d64f2b72eb267d0
TedDraws <edwards730@hotmail.com>
2021-07-29 21:49:50 -0600

    Version 0.2
    Working on adding new memes.

---
+++ Source/MemesPlus.cs
@@ -0,0 +1,19 @@
+using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace MemesPlus.Thoughts
+    {
+        public class Worker_Violence : ThoughtWorker
+        {
+            if (BattleLogEntry_Event = true)
+            {
+                return true;
+            }
+           else
+           {
+               return false;
+           }
+        }
+    }
+
